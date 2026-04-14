3365a5e011090d66df083a3ae59df23be14f30eb
thpir <92860191+thpir@users.noreply.github.com>
2026-04-14 12:38:13 +0200

    Add pinch zoom in image detail view #15

--- lib/ui/features/apod_image_detail/widgets/apod_image_detail_view.dart
+++ lib/ui/features/apod_image_detail/widgets/apod_image_detail_view.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ApodImageDetailView extends StatelessWidget {
+class ApodImageDetailView extends StatefulWidget {
   const ApodImageDetailView({super.key, required this.imageUrl});
 
   final String imageUrl;
 
   @override
+  State<ApodImageDetailView> createState() => _ApodImageDetailViewState();
+}
+
+class _ApodImageDetailViewState extends State<ApodImageDetailView> {
+  final _transformationController = TransformationController();
+
+  @override
+  void dispose() {
+    _transformationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
       color: Colors.black,
-      child: Center(child: Image.network(imageUrl)),
+      child: GestureDetector(
+        onDoubleTap: () =>
+            _transformationController.value = Matrix4.identity(),
+        child: InteractiveViewer(
+          transformationController: _transformationController,
+          boundaryMargin: const EdgeInsets.all(double.infinity),
+          minScale: 0.5,
+          maxScale: 4.0,
+          child: Image.network(
+            widget.imageUrl,
+            width: size.width,
+            height: size.height,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
     );
   }
 }
